71a9770d0c0aeb1a6201dccbe31e8cdd4e5f2343
joaojorges <73176466+joaojorges@users.noreply.github.com>
2020-10-20 13:14:17 -0300

    implementação do construtor e métodos

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,21 @@
-#include <string>
+#include "Vendedor.hpp"
 #include "Empregado.hpp"
+#include <string>
+#define MESES 12
 
-class Vendedor : public Empregado {
+Vendedor::Vendedor(std::string _nome, double _salarioHora, double _quotaMensalVendas){
+  setNome(_nome);
+  setSalarioHora(_salarioHora);
+  setQuotaMensalVendas(_quotaMensalVendas);
+}
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
-	
-};
+double Vendedor::quotaTotalAnual(){
+  return getQuotaMensalVendas() * MESES;
+}
 
+void Vendedor::imprimir(double horas){
+  std::cout << "Nome: " << getNome() << std::endl;
+  std::cout << "Salario Mes: " << pagamentoMes(horas) << std::endl;  
+  std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+  std::cout << std::endl;
+}
