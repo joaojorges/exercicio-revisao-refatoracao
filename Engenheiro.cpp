d34efbab14cc90a7c46a0fbd833178a3ceb306aa
joaojorges <73176466+joaojorges@users.noreply.github.com>
2020-10-20 13:16:00 -0300

    implementação construtor e métodos

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,22 @@
-#include <string>
-#include "Empregado.hpp"
+#include "Engenheiro.hpp"
 
-class Engenheiro : public Empregado {
+Engenheiro::Engenheiro(std::string _nome, double _salarioHora, int _projetos){
+  setNome(_nome);
+  setSalarioHora(_salarioHora);
+  setProjetos(_projetos);
+}
 
-  public:
-    std::string nome;  
-	int projetos;
-	
-};
+int Engenheiro::getProjetos(){
+  return _projetos;
+}
 
+void Engenheiro::setProjetos(int projetos){
+  _projetos = projetos;
+}
+
+void Engenheiro::imprimir(double horas){
+  std::cout << "Nome: " << getNome() << std::endl;
+  std::cout << "Salario Mes: " << pagamentoMes(horas) << std::endl;
+  std::cout << "Projetos: " << getProjetos() << std::endl;
+  std::cout << std::endl;
+}
